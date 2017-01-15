9eaf5b97958256bf3514aaf900d138e0ee548078
云数 <yunshu.wp@alibaba-inc.com>
2017-01-15 22:55:52 +0800

    more convenient

--- generate.js
+++ generate.js
@@ -1,10 +1,9 @@
 var SequelizeAuto = require('sequelize-auto')
-module.exports = function(database, username, password, options) {
+module.exports = function(database, username, password, options, callback) {
   var auto = new SequelizeAuto(database, username, password, options);
 
   auto.run(function(err){
     if (err) throw err;
-    console.log(auto.tables);
-    console.log(auto.foreignKeys);
+    callback(auto)
   })
 }
