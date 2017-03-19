2fd463e6a88468845c8c43beb55cc4a54c79d487
云数 <yunshu.wp@alibaba-inc.com>
2017-03-19 19:06:16 +0800

    copy & paste bugfix

--- main.js
+++ main.js
@@ -15,7 +15,17 @@ const template = [{
      label: `About ${name}`,
       role: 'about'
     }]
-}]
+}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}]
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
